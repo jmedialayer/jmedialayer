cf3ec054cc070ade1cb4c8b5200e275c09288de3
Carlos Ballesteros Velasco <soywiz@gmail.com>
2016-09-19 05:04:19 +0200

    Small fixes

--- jmedialayer-gradle-plugin/src/jmedialayer/extractArchive.kt
+++ jmedialayer-gradle-plugin/src/jmedialayer/extractArchive.kt
@@ -1,23 +1,86 @@
 package jmedialayer
 
 import com.jtransc.error.invalidOp
-import org.codehaus.plexus.archiver.tar.TarBZip2UnArchiver
-import org.codehaus.plexus.archiver.tar.TarGZipUnArchiver
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream
 import org.codehaus.plexus.archiver.zip.ZipUnArchiver
-import java.io.File
+import org.gradle.api.tasks.OutputFile
+import java.io.*
+import java.nio.file.Files
+import java.nio.file.Paths
 
 fun extractArchive(archive: File, outputFolder: File) {
-	val unarchiver = if (archive.path.toLowerCase().endsWith(".zip")) {
-		ZipUnArchiver()
-	} else if (archive.path.toLowerCase().endsWith(".tar.gz")) {
-		TarGZipUnArchiver()
-	} else if (archive.path.toLowerCase().endsWith(".tar.bz2")) {
-		TarBZip2UnArchiver()
+	val lowname = archive.name.toLowerCase()
+	if (lowname.endsWith(".zip")) {
+		uncompressZip(archive, outputFolder)
+	} else if (lowname.endsWith(".tar.gz") || lowname.endsWith(".tar.bz2") || lowname.endsWith(".tar")) {
+		uncompressTar(archive, outputFolder)
 	} else {
 		invalidOp("Don't know how to extract $archive to $outputFolder")
 	}
+}
 
-	unarchiver.sourceFile = archive
-	unarchiver.destDirectory = outputFolder
-	unarchiver.extract()
+fun uncompressZip(archive: File, outputFolder: File) {
+	//ZipUnArchiver(archive).extract("", dest)
+
+	/*
+	FileInputStream(archive).use { fileIS ->
+		val bfileIS = BufferedInputStream(fileIS)
+		ZipArchiveInputStream(bfileIS).use { tarIn ->
+			var entry = tarIn.nextZipEntry
+			while (entry != null) {
+				val destPath = File(dest, entry.name)
+				if (entry.isDirectory) {
+					destPath.mkdirs()
+				} else {
+					destPath.parentFile.mkdirs()
+					BufferedOutputStream(FileOutputStream(destPath)).use { tarIn.copyTo(it) }
+				}
+				entry = tarIn.nextZipEntry
+			}
+		}
+	}
+	*/
+
+	ZipUnArchiver(archive).extract("", outputFolder)
+}
+
+fun uncompressTar(archive: File, outputFolder: File) {
+	//dest.mkdirs()
+
+	/*
+	FileInputStream(archive).use { fileIS ->
+		val bfileIS = BufferedInputStream(fileIS)
+		val uncompressedIs = if (archive.name.toLowerCase().endsWith(".bz2")) {
+			BZip2CompressorInputStream(bfileIS)
+		} else if (archive.name.toLowerCase().endsWith(".gz")) {
+			GzipCompressorInputStream(bfileIS)
+		} else if (archive.name.toLowerCase().endsWith(".tar")) {
+			bfileIS
+		} else {
+			invalidOp("Don't know how to uncompress $archive")
+		}
+		TarArchiveInputStream(uncompressedIs).use { tarIn ->
+			var entry = tarIn.nextTarEntry
+			while (entry != null) {
+				val destPath = File(dest, entry.name)
+				if (entry.isDirectory) {
+					destPath.mkdirs()
+				} else {
+					destPath.parentFile.mkdirs()
+					BufferedOutputStream(FileOutputStream(destPath)).use { tarIn.copyTo(it) }
+				}
+				Files.setPosixFilePermissions(Paths.get(destPath.toURI()),
+					Set<PosixFilePermission> perms)
+				entry.mode
+				entry = tarIn.nextTarEntry
+			}
+		}
+	}
+	*/
+
+	outputFolder.mkdirs()
+	ProcessBuilder("tar", "-xvf", archive.absolutePath, "-C", outputFolder.absolutePath, "--strip", "1").inheritIO().start().waitFor()
 }
